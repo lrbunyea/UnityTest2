262fadf30154f465c29e0c32ea9435874a0e9145
Leo Bunyea <lrbunyea@wpi.edu>
2020-07-03 15:53:32 -0400

    Changed debug message

--- DemoProject/Assets/Scripts/GameManager.cs
+++ DemoProject/Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Game Manager has started.");
+        Debug.Log("Game Manager has initiated.");
     }
 
     // Update is called once per frame
