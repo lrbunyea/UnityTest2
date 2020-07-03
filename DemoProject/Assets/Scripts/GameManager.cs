2912772b2fdc0c3991dc9f3576b6eddf41aec9ec
Leo Bunyea <lrbunyea@wpi.edu>
2020-07-03 16:09:16 -0400

    Dummy log message

--- DemoProject/Assets/Scripts/GameManager.cs
+++ DemoProject/Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         Debug.Log("Game Manager has initiated.");
+        Debug.Log("Wolves are cool.");
     }
 
     // Update is called once per frame
